827fa2deb9ad6900c921923ff6ca4d3ed61a0c5a
Joon <joon@slatedev.com>
2019-06-11 23:16:58 -0600

    enum

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -2,5 +2,5 @@ class Accessory < ApplicationRecord
   has_many :accessory_products
   has_many :products, through: :accessory_product
 
-  enum size: [:small, :medium, :large]
+  enum size: [:small, :medium, :large, :all]
 end
